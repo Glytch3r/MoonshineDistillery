5bc56223ddd75af23b52a2607769543f63365411
Glytch3r <glytch3r.arts@gmail.com>
2025-03-06 22:13:24 +0800

    init

---
+++ media/lua/client/MoonshineDistiller.lua
@@ -0,0 +1,330 @@
+--[[██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                        				 Custom  PZ  Mod  Developer  for  Hire													  |
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                       	Portfolio:  https://steamcommunity.com/id/glytch3r/myworkshopfiles/							          |
+|                       		                                    														 	  |
+|                       	Discord:    Glytch3r#1337 / glytch3r															      |
+|                       		                                    														 	  |
+|                       	Support:    https://ko-fi.com/glytch3r														    	  |
+|_______________________________________________________________________________________________________________________________-]]
+--[[_____________________________________________________________________________________________________________________________
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████--]]
+
+
+MoonshineDistillery = MoonshineDistillery or {}
+-----------------------            ---------------------------
+function MoonshineDistillery.loadCursor()
+   ISMoonshineTileCursor = ISBuildingObject:derive("ISMoonshineTileCursor")
+
+   function ISMoonshineTileCursor:create(x, y, z, sprite)
+      local pl = getPlayer()
+      local positions = {}
+      if sprite == "MoonshineDistillery_16" then
+         positions = {
+               {x, y, z, "MoonshineDistillery_16"},
+               {x + 1, y, z, "MoonshineDistillery_17"},
+               {x + 1, y - 1, z, "MoonshineDistillery_18"},
+               {x + 2, y - 1, z, "MoonshineDistillery_19"}
+         }
+      else
+         positions = {
+               {x, y, z, "MoonshineDistillery_27"},
+               {x, y + 1, z, "MoonshineDistillery_26"},
+               {x - 1, y + 1, z, "MoonshineDistillery_25"},
+               {x - 1, y + 1, z, "MoonshineDistillery_24"}
+         }
+      end
+      local firstSq = getCell():getOrCreateGridSquare(x, y, z);
+      if not MoonshineDistillery.checkDist(pl, firstSq) then
+         pl:setHaloNote(tostring("Need to get closer"),150,250,150,900)
+         return
+      end
+      for _, pos in ipairs(positions) do
+         local square = getCell():getGridSquare(pos[1], pos[2], pos[3])
+         if not square or not square:isFree(false) then
+            pl:setHaloNote(tostring("Cannot place there"),150,250,150,900)
+            return
+         end
+      end
+
+
+      for _, pos in ipairs(positions) do
+         local square = getCell():getGridSquare(pos[1], pos[2], pos[3])
+         if square then
+            local props = ISMoveableSpriteProps.new(IsoObject.new(square, pos[4]):getSprite())
+            props.rawWeight = 10
+            props:placeMoveableInternal(square, InventoryItemFactory.CreateItem("Base.Plank"), pos[4])
+            addSound(pl, square:getX(), square:getY(), square:getZ(), 5, 1)
+            getSoundManager():PlayWorldSound('MoonshineBuild', square, 0, 5, 5, false)
+         end
+      end
+
+
+
+   end
+
+   function ISMoonshineTileCursor:render(x, y, z, square)
+      ISBuildingObject.render(self, x, y, z, square)
+   end
+
+   function ISMoonshineTileCursor:new(sprite, character, firstSq)
+      local o = {}
+      setmetatable(o, self)
+      self.__index = self
+      o:init()
+      o:setSprite(sprite)
+      o.character = character
+      o.player = character:getPlayerNum()
+      o.isTileCursor = true
+      o.spriteName = sprite
+      o.noNeedHammer = true
+      o.skipBuildAction = false
+      o.skipWalk2 = false
+      o.canBeAlwaysPlaced = true
+      return o
+   end
+end
+Events.OnCreatePlayer.Add(MoonshineDistillery.loadCursor)
+--   MoonshineDistillery.loadCursor()
+-----------------------            ---------------------------
+function MoonshineDistillery.CanPlace(pl)
+   local inv = pl:getInventory()
+   return inv:contains("MoonDist.Boiler") and inv:contains("MoonDist.Thumper")-- and inv:contains("MetalDrum")
+end
+-----------------------
+function MoonshineDistillery.hasThermometer(pl)
+   local inv = pl:getInventory()
+   return inv:contains("MoonDist.Thermometer")
+end
+function MoonshineDistillery.hasDrainPort(pl)
+   local inv = pl:getInventory()
+   return inv:contains("MoonDist.DrainPort")
+end
+function MoonshineDistillery.hasStillCap(pl)
+   local inv = pl:getInventory()
+   return inv:contains("MoonDist.StillCap")
+end
+-----------------------
+function MoonshineDistillery.hasMetalDrum(pl)
+   local inv = pl:getInventory()
+   return inv:contains("Base.MetalDrum") or inv:contains("Moveables.industry_01_22") or inv:contains("Moveables.industry_01_23")
+end
+-----------------------            ---------------------------
+function MoonshineDistillery.getThermometer(pl)
+   local inv = pl:getInventory()
+   return inv:FindAndReturn("MoonDist.Thermometer")
+end
+function MoonshineDistillery.getDrainPort(pl)
+   local inv = pl:getInventory()
+   return inv:FindAndReturn("MoonDist.DrainPort")
+end
+function MoonshineDistillery.getStillCap(pl)
+   local inv = pl:getInventory()
+   return inv:FindAndReturn("MoonDist.StillCap")
+end
+-----------------------
+function MoonshineDistillery.getMetalDrum(pl)
+   local inv = pl:getInventory()
+   return inv:FindAndReturn("MoonDist.MetalDrum") or inv:FindAndReturn("Moveables.industry_01_22") or inv:FindAndReturn("Moveables.industry_01_23")
+end
+-----------------------            ---------------------------
+MoonshineDistillery.addParts = {
+   ["MoonshineDistillery_16"] = {["StillCap"] = "MoonshineDistillery_20", ["Thermometer"] = "MoonshineDistillery_21"},
+   ["MoonshineDistillery_27"] = {["StillCap"] = "MoonshineDistillery_28", ["Thermometer"] = "MoonshineDistillery_29"},
+   ["MoonshineDistillery_19"] = {["DrainPort"]= "MoonshineDistillery_22"},
+   ["MoonshineDistillery_24"] = {["DrainPort"]= "MoonshineDistillery_30"},
+}
+
+function MoonshineDistillery.getOverlayToAdd(sprName, part)
+   return MoonshineDistillery.addParts[sprName].part
+end
+function MoonshineDistillery.addOverlay(obj, sprName, part, item)
+   if not (obj or sprName) then return end
+   local overlay = MoonshineDistillery.getOverlayToAdd(sprName, part)
+   if overlay then
+      getPlayer():getInventory():remove(item)
+      obj:setOverlaySprite(tostring(overlay), 1,1,1,1)
+   end
+end
+---------------------------
+function MoonshineDistillery.isCampfire(sprName)
+   local tab = {
+      ["camping_01_6"]=true,
+      ["camping_01_5"]=true,
+      ["camping_01_4"]=true,
+   }
+   return tab[sprName]
+end
+-----------------------
+function MoonshineDistillery.isBoilerTile(sprName)
+   local tab = {
+      ["MoonshineDistillery_16"] = true,
+      ["MoonshineDistillery_27"] = true
+   }
+   return tab[sprName]
+end
+
+
+function MoonshineDistillery.isCondenserCanTile(sprName)
+   local tab = {
+      ["MoonshineDistillery_19"] = true,
+      ["MoonshineDistillery_24"] = true
+   }
+   return tab[sprName]
+end
+
+function MoonshineDistillery.hasThermometerOverlay(obj)
+   if obj:getOverlaySprite() ~= nil then
+      local sprName = obj:getOverlaySprite():getName()
+      if sprName then
+         local tab = {
+            ["MoonshineDistillery_21"] = true,
+            ["MoonshineDistillery_29"] = true
+         }
+         return tab[sprName]
+      end
+   end
+   return false
+end
+
+function MoonshineDistillery.hasStillCapOverlay(obj)
+   if obj:getOverlaySprite() ~= nil then
+      local sprName = obj:getOverlaySprite():getName()
+      if sprName then
+         local tab = {
+            ["MoonshineDistillery_20"] = true,
+            ["MoonshineDistillery_28"] = true
+         }
+         return tab[sprName]
+      end
+   end
+   return false
+end
+
+function MoonshineDistillery.hasDrainPortOverlay(obj)
+   if obj:getOverlaySprite() ~= nil then
+      local sprName = obj:getOverlaySprite():getName()
+      if sprName then
+         local tab = {
+            ["MoonshineDistillery_22"] = true,
+            ["MoonshineDistillery_30"] = true
+         }
+         return tab[sprName]
+      end
+   end
+   return false
+end
+
+-----------------------            ---------------------------
+
+function MoonshineDistillery.checkDist(pl, sq)
+	local x, y = sq:getX(), sq:getY()
+	local dist = pl:DistTo(x, y)
+   return math.floor(dist) <= 2
+end
+
+function MoonshineDistillery.context(player, context, worldobjects, test)
+   local pl = getSpecificPlayer(player)
+   local inv = pl:getInventory()
+   local sq = clickedSquare
+
+   if sq then
+      if MoonshineDistillery.CanPlace(pl) then
+         local Main = context:addOptionOnTop("Build Moonshine Distiller: ")
+         Main.iconTexture = getTexture("media/ui/chop_tree.png")
+         local opt = ISContextMenu:getNew(context)
+         context:addSubMenu(Main, opt)
+
+         opt:addOption('South', worldobjects, function()
+            local cursor = ISMoonshineTileCursor:new("MoonshineDistillery_16",  pl, sq)
+            getCell():setDrag(cursor, 0)
+         end)
+
+         opt:addOption('East', worldobjects, function()
+            local cursor = ISMoonshineTileCursor:new("MoonshineDistillery_27",  pl, sq)
+            getCell():setDrag(cursor, 0)
+         end)
+      end
+
+      for i=0, sq:getObjects():size()-1 do
+         local obj = sq:getObjects():get(i)
+         local spr = obj:getSprite()
+         if spr then
+            local sprName = spr:getName()
+            if sprName then
+
+
+               if MoonshineDistillery.isCampfire(sprName) then
+                  if MoonshineDistillery.hasMetalDrum(pl) then
+                     opt:addOption('Build Cooking Vat', worldobjects, function()
+                        local item = MoonshineDistillery.getMetalDrum(pl)
+                        if item then
+                           inv:remove(item)
+                           obj:setOverlaySprite(tostring("MoonshineDistillery_1"), 1,1,1,1)
+                        end
+                     end)
+                  end
+               end
+
+
+               if MoonshineDistillery.isBoilerTile(sprName) then
+                  if MoonshineDistillery.hasThermometer(pl) then
+                     if not MoonshineDistillery.hasThermometerOverlay(obj) then
+                        context:addOptionOnTop('Add Thermometer', worldobjects, function()
+                           local part = "Thermometer"
+                           local item = MoonshineDistillery.getThermometer(pl)
+                           if item then
+                              MoonshineDistillery.addOverlay(obj, sprName, part, item)
+                           end
+                        end)
+                     end
+                  end
+                  if MoonshineDistillery.hasStillCap(pl) then
+                     if not MoonshineDistillery.hasStillCapOverlay(obj) then
+                        context:addOptionOnTop('Add Stiil Cap', worldobjects, function()
+                           local part = "StillCap"
+                           local item = MoonshineDistillery.getStillCap(pl)
+                           if item then
+                              MoonshineDistillery.addOverlay(obj, sprName, part, item)
+                           end
+                        end)
+                     end
+                  end
+               elseif MoonshineDistillery.isCondenserCanTile(sprName) then
+                  if MoonshineDistillery.hasDrainPort(pl) then
+                     if not MoonshineDistillery.hasDrainPortOverlay(obj) then
+                        context:addOptionOnTop('Add Drain Port', worldobjects, function()
+                           local part = "DrainPort"
+                           local item = MoonshineDistillery.getDrainPort(pl)
+                           if item then
+                              MoonshineDistillery.addOverlay(obj, sprName, part, item)
+                           end
+                        end)
+                     end
+                  end
+               end
+            end
+         end
+      end
+   end
+end
+
+Events.OnFillWorldObjectContextMenu.Remove(MoonshineDistillery.context)
+Events.OnFillWorldObjectContextMenu.Add(MoonshineDistillery.context)
+
+-----------------------            ---------------------------
---
+++ media/lua/client/MoonshineDistiller_Container.lua
@@ -0,0 +1,98 @@
+--[[██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                        				 Custom  PZ  Mod  Developer  for  Hire													  |
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                       	Portfolio:  https://steamcommunity.com/id/glytch3r/myworkshopfiles/							          |
+|                       		                                    														 	  |
+|                       	Discord:    Glytch3r#1337 / glytch3r															      |
+|                       		                                    														 	  |
+|                       	Support:    https://ko-fi.com/glytch3r														    	  |
+|_______________________________________________________________________________________________________________________________-]]
+--[[_____________________________________________________________________________________________________________________________
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████--]]
+
+
+
+MoonshineDistillery = MoonshineDistillery or {}
+--[[
+function MoonshineDistillery.doRoll()
+    local chance = SandboxVars.Moonshine.DeadlyBatchChance
+    if chance == 0 then return false end
+    if chance == 100 then return true end
+    return percent >= ZombRand(1, 101)
+end
+
+
+function MoonshineDistillery.getWaitPeriod()
+    local period = SandboxVars.Moonshine.WaitPeriod
+    return period or 4
+end
+
+
+function MoonshineDistillery.getYield(obj)
+    local pl = getPlayer()
+    if not obj then return end
+    local safeLvl = SandboxVars.Moonshine.SafeBatchLevel
+    local lvl = pl:getPerkLevel(Perks.Cooking)
+    local roll = MoonshineDistillery.doRoll()
+    local cont = obj:getContainer()
+    local contInv = cont and cont:getItems()
+
+    local min = SandboxVars.Moonshine.MinPossibleYield or 0
+    local max = SandboxVars.Moonshine.MaxPossibleYield or 6
+    local bonus = SandboxVars.Moonshine.CookLevelBonus or true
+    local yield = ZombRand(min, max+1)
+    local item = "Base.Moonshine"
+    if bonus then
+        yield = yield + lvl
+    end
+    if lvl <= safeLvl and roll then
+        item = "Base.DeadlyMoonshine"
+    end
+    if yield > 0 then
+        cont:AddItems(item, yield)
+    end
+
+    cont:setDrawDirty(true);
+
+end
+
+ ]]
+function MoonshineDistillery.delItem(cont, fType, qty)
+    qty = qty or 1
+    local count = cont:getItemCount(fType)
+    --local count = item:getContainer():getNumberOfItem(fType)
+    if count >= qty then
+        for i=1, qty do
+            local item = contInv:FindAndReturn(fType)
+            if item then
+                if isClient() then
+                    cont:removeItemOnServer(item)
+                end
+                cont:DoRemoveItem(item)
+            end
+        end
+        getPlayerLoot(0):refreshBackpacks()
+    end
+end
+
+-----------------------            ---------------------------
+function MoonshineDistillery.isLearned(pl)
+    local recipe = "Build Moonshine Distiller"
+    pl = pl or getPlayer()
+    return pl:getKnownRecipes():contains(recipe) or pl:isRecipeKnown(recipe)
+end
